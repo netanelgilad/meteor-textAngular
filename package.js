93f246f6a07e70717b9bd266ca4568c679dfffbf
David Yahalomi <davidyaha@gmail.com>
2015-03-10 21:52:42 +0200

    Removed requirement for bower package

--- package.js
+++ package.js
@@ -1,17 +1,34 @@
 Package.describe({
   summary: "A radically powerful Text-Editor/Wysiwyg editor for Angular.js!",
-  version: "0.0.4",
-  git: "https://github.com/netanelgilad/meteor-textAngular.git"
+  version: "0.0.5",
+  git: "https://github.com/netanelgilad/meteor-textAngular.git",
+  name: "netanelgilad:text-angular"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0.1');
-  api.use('mquandalle:bower@0.1.11', 'client');
-  api.use('urigo:angular@0.4.5', 'client');
+  api.use('urigo:angular@0.7.2', 'client');
 
-  api.addFiles('bower.json', 'client');
+  api.addFiles(["bower_components/bootstrap-css-only/css/bootstrap.css",
+                "bower_components/bootstrap-css-only/fonts/glyphicons-halflings-regular.eot",
+                "bower_components/bootstrap-css-only/fonts/glyphicons-halflings-regular.svg",
+                "bower_components/bootstrap-css-only/fonts/glyphicons-halflings-regular.ttf",
+                "bower_components/bootstrap-css-only/fonts/glyphicons-halflings-regular.woff",
+                "bower_components/bootstrap-css-only/fonts/glyphicons-halflings-regular.woff2"] , 'client');
 
-  api.addFiles('.meteor/local/bower/textAngular/dist/textAngular-sanitize.min.js', 'client');
+  api.addFiles(['bower_components/font-awesome/css/font-awesome.css',
+                'bower_components/font-awesome/fonts/FontAwesome.otf',
+                'bower_components/font-awesome/fonts/fontawesome-webfont.eot',
+                'bower_components/font-awesome/fonts/fontawesome-webfont.svg',
+                'bower_components/font-awesome/fonts/fontawesome-webfont.ttf',
+                'bower_components/font-awesome/fonts/fontawesome-webfont.woff',
+                'bower_components/font-awesome/fonts/fontawesome-webfont.woff2'], 'client');
+
+  api.use('maxharris9:rangy', 'client');
+
+  api.addFiles(['bower_components/textAngular/dist/textAngular-rangy.min.js',
+                'bower_components/textAngular/dist/textAngular-sanitize.min.js',
+                'bower_components/textAngular/dist/textAngular.min.js'], 'client');
 
   // Client files.
   api.addFiles('init.js', 'client');
@@ -19,6 +36,6 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('netanelgilad:text-angular');
-  api.addFiles('netanelgilad:text-angular-tests.js');
+  api.use('netanelgilad:text-angular', 'client');
+  api.addFiles('netanelgilad:text-angular-tests.js', 'client');
 });
